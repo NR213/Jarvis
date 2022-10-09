bd863188d67e748a0a1fab298888ef3ba0e857b1
Nishanth Raikar <39913541+NR213@users.noreply.github.com>
2022-10-09 20:27:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,91 +1,57 @@
 import codecs
 import io
 from pyexpat.errors import codes
+from flask import Response
 from urllib import request
-
 from flask import Flask, jsonify, render_template,request, redirect
-#from utils import predict
+from flask_ngrok import run_with_ngrok
 import csv
 import pandas as pd
 import json
 
+def create_app():
+  app = Flask(__name__, instance_relative_config=True)
+  run_with_ngrok(app) 
 
-app = Flask(__name__)
-  
+  @app.route('/', methods=['GET','POST'])
+  def validate_file():
+      json_output = {}
+      try:
+          excel_data = request.json['dataQueries'] 
+          questions = request.json['questions']
+          if not excel_data:
+              raise Exception('No proper data or query found!')
+          queryString = ''
+          queryString += '|'.join([data['name'].split('.')[1] for data in excel_data])
+          data_headers = [data['name'].split('.')[1] for data in excel_data]
+          row_values = []
+          rows = []
+          count = 0
+          while count < len(excel_data[0]["data"]):
+              for each in excel_data:
+                  for index,value in enumerate(each['data']):
+                      if index == count:
+                          row_values.append(value)
+              row_list = {f"row_{count}":row_values}
+              row_values = []
+              rows.append(row_list)
+              count = count + 1
+          
+          for index, data in enumerate(rows):
+              queryString += '\n'
+              queryString += '|'.join([str(element) for element in data[f'row_{index}']])
+          queryString = '"""'+ queryString + '"""'
+          
+          result = predict(queryString,questions)
 
-@app.route('/Jarvis', methods=['GET'])
-def upload_file():
-    """ Simple html file to pass data """
-    return render_template('upload_file.html')
-
-@app.route('/Jarvis', methods=['POST'])
-def validate_file():
-    """ Validates the data and the query and populates the result """
-    try:
-        if 'file' not in request.files:
-            raise Exception('File not uploaded properly')
-        
-        file = json.load(request.files.get('file'))
-
-        excel_data = file['dataQueries'] 
-        questions = file['questions']
-        if not excel_data:
-            raise Exception('No proper data or query found!')
-
-        queryString = ''
-        queryString += '|'.join([data['name'].split('.')[1] for data in excel_data])
-        data_headers = [data['name'].split('.')[1] for data in excel_data]
-
-        row_values = []
-        rows = []
-        count = 0
-        while count < len(excel_data[0]["data"]):
-            for each in excel_data:
-                for index,value in enumerate(each['data']):
-                    if index == count:
-                        row_values.append(value)
-            row_list = {f"row_{count}":row_values}
-            row_values = []
-            rows.append(row_list)
-            count = count + 1
-        
-        for index, data in enumerate(rows):
-            queryString += '\n'
-            queryString += '|'.join([str(element) for element in data[f'row_{index}']])
-
-        print(queryString)
-        print(questions)
-
-        #result = predict(queryString,questions)
-
-
-        
-        
-        validated_result = {
-                            'status': 200,
-                            'data': 'success',
-                            'Error': None
-                            }
-        
-        return render_template('show_data.html', validated_result=validated_result)
-    except Exception as e:
-        print(e)
-        validated_result = {'status': 404,
-                                    'data': None,
-                                    'Error': 'Error with data!'
-                            }
-        
-        # message = e.args[0] 
-        # if message:
-        #     validated_result = jsonify({'status': 404,
-        #                                 'data': None,
-        #                                 'Error': message
-        #                                 })
-        
-        print(validated_result)
-            
-        return render_template('show_data.html', validated_result=validated_result)
-        
-
-if __name__ == "__main__":
-    app.run(debug=True)
+          result = result.rstrip(result[-1])
+          ans = result.split("|")
+          
+          json_output['answer'] = ans
+          
+          return Response(json_output['answer'], status=200, mimetype='application/json')
+      
+      except Exception as e:
+          json_output['answer'] = ''
+          return Response(json_output['answer'], status=404, mimetype='application/json')
+  return app
